21760f12b1a6bdfb6b3dad72a49a24b9af782aa3
Friiqi <friiqi@gmail.com>
2020-02-06 11:08:51 +0200

    volley post works now, req.url has registertoken, and body has idtoken.

--- app/src/main/java/com/example/mobice/LoginActivity.java
+++ app/src/main/java/com/example/mobice/LoginActivity.java
@@ -1,32 +1,54 @@
 package com.example.mobice;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 123;
+    private static final String TAG = "regtoken";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginactivity);
         createSignInIntent();
+
     }
 
     public void createSignInIntent() {
@@ -43,7 +65,160 @@ public class LoginActivity extends AppCompatActivity {
                         .build(),
                 RC_SIGN_IN);
         // [END auth_fui_create_intent]
+
     }
+
+    public void getTokens() {
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                       final String token = task.getResult().getToken();
+
+                        // Log and toast
+                       // String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d(TAG, token);
+                        Toast.makeText(LoginActivity.this, token, Toast.LENGTH_SHORT).show();
+
+                        FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
+                        mUser.getIdToken(true)
+                                .addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
+                                    public void onComplete(@NonNull Task<GetTokenResult> task) {
+                                        if (task.isSuccessful()) {
+                                            String idToken = task.getResult().getToken();
+                                            //Toast.makeText(getApplicationContext(),token,Toast.LENGTH_LONG).show();
+
+                                            try {
+                                                volleyPost(token, idToken);
+                                            } catch (AuthFailureError authFailureError) {
+                                                authFailureError.printStackTrace();
+                                                Toast.makeText(getApplicationContext(),"error sending userIdToken",Toast.LENGTH_LONG).show();
+                                            }
+
+                                            // ...
+                                        } else {
+                                            Toast.makeText(getApplicationContext(),"error sending userIdToken",Toast.LENGTH_LONG).show();
+                                        }
+                                    }
+
+                                });
+
+                    }
+                });
+    }
+
+    public void volleyPost(String regtoken, final String idToken) throws AuthFailureError {
+
+        String url = "http://o202.nor.fi:3000/" + regtoken; // <----enter your post url here
+
+        final StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                //This code is executed if the server responds, whether or not the response contains data.
+                //The String 'response' contains the server's response.
+            }
+        }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //This code is executed if there is an error.
+                Log.d("appErr", error.toString());
+            }
+
+        }) {
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+
+                return idToken.getBytes();
+
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/text");
+                headers.put("charset", "utf-8");
+                return headers;
+            }
+        };
+
+
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("idToken", idToken);
+            //if i want i could send register token in body as well, but then i would need to handle it in backend (they end up in same file)
+            //obj.put("regToken", regtoken);
+            Log.d("idToken", obj.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        final String requestBody = obj.toString();
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                url, obj,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Do something with response
+                        //mTextView.setText(response.toString());
+
+                        // Process the JSON
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Do something when error occurred
+                        //Toast.makeText(getApplicationContext(),"error ",Toast.LENGTH_LONG).show();
+                        Log.d("ERR: ", String.valueOf(error));
+                    }
+                }) {
+            @Override
+            public byte[] getBody() {
+                try {
+                    return requestBody == null ? null : requestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s",
+                            requestBody, "utf-8");
+                    return null;
+                }
+            }
+        };
+        Log.d("body: ", MyStringRequest.getBody().toString());
+        String testi = new String(jsonObjectRequest.getBody());
+        Log.d("bodytesti: ", testi);
+        Log.d("bodytesti2: ",testi );
+        RequestQueue MyRequestQueue = Volley.newRequestQueue(this);
+        MyRequestQueue.add(jsonObjectRequest);
+
+    }
+
+//    public void idTokenToBackEnd() {
+//        FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
+//        mUser.getIdToken(true)
+//                .addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
+//                    public void onComplete(@NonNull Task<GetTokenResult> task) {
+//                        if (task.isSuccessful()) {
+//                            String idToken = task.getResult().getToken();
+//                           volleyPost(idToken);
+//                            Log.d("idToken: ",idToken);
+//                            // ...
+//                        } else {
+//                            Toast.makeText(getApplicationContext(),"error sending userIdToken",Toast.LENGTH_LONG).show();
+//                        }
+//                    }
+//
+//                });
+//    }
+
 
     // [START auth_fui_result]
     @Override
@@ -56,6 +231,10 @@ public class LoginActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+               // idTokenToBackEnd();
+                getTokens();
+
+
                 // ...
             } else {
                 if (data == null){
--- app/src/main/java/com/example/mobice/MainActivity.java
+++ app/src/main/java/com/example/mobice/MainActivity.java
@@ -1,34 +1,26 @@
 package com.example.mobice;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.iid.InstanceIdResult;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.os.Build;
-import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.RequestQueue;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
-import com.google.firebase.messaging.FirebaseMessaging;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnLogin;
+    private Button btnLogin,btn2;
+    public String token2;
+    private RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,19 +30,46 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSuccess(InstanceIdResult instanceIdResult) {
                 String token = instanceIdResult.getToken();
-                Log.d("MyFirebaseToken", token);
+                 token2 = instanceIdResult.getToken();
 
+                Log.d("MyFirebaseToken", token);
+               // volleyPost(token);
             }});
         btnLogin = findViewById(R.id.btnLogin);
+        btn2 =findViewById(R.id.btn2);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openLogin();
+
+
             }
         });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //sendMessage(token2);
+                String data = "{"+
+                        "\"token\"" + "\"" + token2 + "\","+
+
+                        "}";
+                //Submit(data);
+                //Submit(token2);
+
+
+            }
+        });
+
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        if (currentUser != null){
-            btnLogin.setActivated(false);
+
+        if (currentUser == null){
+            //btnLogin.setActivated(false);
+            openLogin();
+        }
+        else {
+            Toast.makeText(getApplicationContext(),"user uid: " +currentUser.getUid(),Toast.LENGTH_LONG).show();
         }
     }
 
@@ -59,36 +78,68 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-/*
-    public void getToken(View v) {
-        // Get token
-        // [START retrieve_current_token]
-        FirebaseInstanceId.getInstance().getInstanceId()
-                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
-                        if (!task.isSuccessful()) {
-                            Log.w(TAG, "getInstanceId failed", task.getException());
-                            return;
-                        }
 
-                        // Get new Instance ID token
-                        String token = task.getResult().getToken();
 
-                        // Log and toast
-                        String msg = getString(R.string.msg_token_fmt, token);
-                        Log.d(TAG, msg);
-                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-                    }
-                });
-    }
 
-    public void onNewToken(String token) {
-        Log.d(TAG, "Refreshed token: " + token);
+//    private void Submit(String data)
+//    {
+//        final String savedata= data;
+//        String URL="http://o202.nor.fi:3000/"+data;
+//
+//        requestQueue = Volley.newRequestQueue(getApplicationContext());
+//        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+//            @Override
+//            public void onResponse(String response) {
+//                try {
+//                    JSONObject objres=new JSONObject(response);
+//                    Toast.makeText(getApplicationContext(),objres.toString(),Toast.LENGTH_LONG).show();
+//
+//
+//                } catch (JSONException e) {
+//                    Toast.makeText(getApplicationContext(),"Server Error",Toast.LENGTH_LONG).show();
+//
+//                }
+//                //Log.i("VOLLEY", response);
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//
+//                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+//
+//                //Log.v("VOLLEY", error.toString());
+//            }
+//        }) {
+//            @Override
+//            public String getBodyContentType() {
+//                return "application/json; charset=utf-8";
+//            }
+//
+//            @Override
+//            public byte[] getBody() throws AuthFailureError {
+//                try {
+//                    return savedata == null ? null : savedata.getBytes("utf-8");
+//                } catch (UnsupportedEncodingException uee) {
+//                    //Log.v("Unsupported Encoding while trying to get the bytes", data);
+//                    return null;
+//                }
+//            }
+//
+//        };
+//        requestQueue.add(stringRequest);
+//    }
 
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // Instance ID token to your app server.
-        sendRegistrationToServer(token);
-    }*/
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
