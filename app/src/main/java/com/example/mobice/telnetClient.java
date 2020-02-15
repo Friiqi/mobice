fc7bc0d8f29923abd2ab65f9d1fb37d89aaff626
Friiqi <friiqi@gmail.com>
2020-02-15 15:26:48 +0200

    socket connection no longer crashes app, and makes a connection to Icesus. Need to figure out how to pass data between app and Icesus and show results etc

--- app/src/main/java/com/example/mobice/telnetClient.java
+++ app/src/main/java/com/example/mobice/telnetClient.java
@@ -2,41 +2,66 @@ package com.example.mobice;
 
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.EditText;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class telnetClient extends AsyncTask<String,Void,String> {
-
+    Socket s;
+    InputStreamReader in;
+    BufferedReader bufferedReader;
+    public String str = "mm";
+    public EditText txtV ;
 
     @Override
     protected String doInBackground(String... strings) {
-        Socket s;
-        InputStreamReader in;
-        BufferedReader bufferedReader;
-        String str;
+
+//        new Thread(new Runnable() {
+//            public void run() {
+//                //open socket
+//            }
+//        }).start();
+       return null;
+}
 
 
-        try {
-            s = new Socket("95.216.243.162", 4000);
-            s.close();
-            return "connection estabilished";
-//            PrintWriter pWrite = new PrintWriter(s.getOutputStream());
-//            pWrite.println("ahab+ testi");
-//            pWrite.flush();
-//            in = new InputStreamReader(s.getInputStream());
-//            bufferedReader = new BufferedReader(in);
-//            str = bufferedReader.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
+        public void openSocket() {
+            //txtV= telnetTab.txtTelnet;
+            new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        s = new Socket("icesus.org", 4000);
+                        //s.close();
+                        Log.d("socket", "success");
+
+            PrintWriter pWrite = new PrintWriter(s.getOutputStream());
+            pWrite.println("supasecret\r");
+            pWrite.flush();
+            in = new InputStreamReader(s.getInputStream());
+            bufferedReader = new BufferedReader(in);
+            while(s.isConnected()) {
+                str = bufferedReader.readLine();
+
+                //telnetTab.telString = str;
+                Log.d("socket string", str);
+            }
+
+                    } catch (IOException e) {
+                        Log.d("socket", "crash");
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
         }
 
 
 
-        return null;
-    }
+
     @Override
     protected void onPostExecute(String result){
         if (result != null) {
